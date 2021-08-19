000a988fab94e3ad01310c2158ed982b61fa93e0
Aimen <aimenhamed@live.com.au>
2021-08-19 21:49:43 +1000

    initial react app

---
+++ src/App.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Text = styled.p`
+  font-size: 18px;
+  line-height: 24px;
+  color: black;
+  font-family: "Space Grotesk";
+`;
+
+export const Heading = styled.h1`
+  font-size: 65px;
+  line-height: 72px;
+  width: max-content;
+  font-family: "Space Grotesk";
+`;
