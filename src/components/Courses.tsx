3dd02b4bbdc7cf44709bffb2f10376f5a544e5df
Aimen <aimenhamed@live.com.au>
2021-08-25 11:38:08 +1000

    refactor

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -2,40 +2,28 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import axios from 'axios';
-import {Text} from '../App.style';
-import { CourseContainer, CourseItem, CourseList, LikeButton } from './Courses.style';
+import { Text } from '../App.style';
+import { CourseItem, LikeButton } from './Courses.style';
 import { BsHeartFill, BsHeart } from 'react-icons/bs';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { isJSDocNullableType } from 'typescript';
-
-const getRandomColor = () => {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
 
 let likedList = [];
 
 export const Courses = () => {
-    const url = "http://127.0.0.1:5000/api/courses";
     const [courses, setCourses] = useState(null);
+    const url = "https://ratecseapi.herokuapp.com/api/courses";
 
     useEffect(() => {
         axios.get(url).then(response => {
             setCourses(response.data)
-        });    
+        });
     }, [courses]);
 
     const likeCourse = (id) => {
-        console.log(likedList);
-        console.log(id);
         if (!likedList[parseInt(id)]) {
-            axios.post(`${url}/likes/${id}`);
+            axios.put(`${url}/likes/${id}`);
             likedList[parseInt(id)] = true;
         } else {
             axios.delete(`${url}/likes/${id}`);
@@ -51,13 +39,13 @@ export const Courses = () => {
                         <CourseItem>
                             <Text id={course.id}>{course.name}</Text>
                             <LikeButton onClick={() => likeCourse(course.id)}>
-                                {likedList[course.id] == true ? 
+                                {likedList[course.id] == true ?
                                     <BsHeartFill color="ff5c5c" size="2rem" />
                                     :
                                     <BsHeart color="ff5c5c" size="2rem" />
                                 }
                                 <Text>{course.likes}</Text>
-                            </LikeButton> 
+                            </LikeButton>
                         </CourseItem>
                     </Col>
                 ))}
